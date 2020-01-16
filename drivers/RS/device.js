bf0ae89ae277040ff02e663264347d5df0cb7501
Momotica <menno@momotica.nl>
2020-01-16 18:27:14 +0100

    Added support for RS-Z Room Sensor

---
+++ drivers/RS/device.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+class DanfossRS extends ZwaveDevice {
+	onMeshInit() {
+
+		//enable debugging
+		this.enableDebug();
+
+		//print the node's info to the console
+		this.printNode();
+
+		this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT', {
+			getOpts: {
+				getOnOnline: true,
+			},
+		});
+		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
+			report: 'SENSOR_MULTILEVEL_REPORT',
+			reportParser: report => {
+				return ((Math.round(report['Sensor Value (Parsed)'] * 10) / 10));
+				
+			},
+			getOpts: {
+				getOnOnline: true,
+			},
+		});
+		this.registerCapability('alarm_battery', 'BATTERY', {
+			getOpts: {
+				getOnOnline: true,
+			},
+		});
+		this.registerCapability('measure_battery', 'BATTERY', {
+			getOpts: {
+				getOnOnline: true,
+			},
+		});
+
+	}
+}
+
+module.exports = DanfossRS;
